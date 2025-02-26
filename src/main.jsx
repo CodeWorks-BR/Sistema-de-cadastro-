309936719c37a04fdb076182445382a908c951b8
ivanizidoro <ivanizidoro.com.br>
2025-02-26 20:03:58 -0300

    feito-criação-dos-stylos-globais

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Login from './login/index.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Login from "./login/index.jsx";
+import GlobalStyle from "./styles/globalStyles.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <GlobalStyle />
     <Login />
-  </StrictMode>,
-)
+  </StrictMode>
+);
